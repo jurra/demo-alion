0fec712a80810039854e70a574ad0ba29fbc46b4
jurra <jose.urra86@gmail.com>
2019-08-04 15:02:09 +0200

    form logic fdone

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -3,16 +3,23 @@
     <h1 class="title is-1 has-text-centered">..so call me maybe</h1>
     <div class="columns">
       <div class="column is-one-third"></div>
-      <div class="column is-two-fifths">
+      <div v-if="!form.submitted" class="column is-two-fifths">
         <div class="field">
           <label class="label">Name</label>
-          <div class="control">
+          <div class="control has-icons-left has-icons-right">
             <input
               v-model="form.name"
-              :class="[nameIsValid ? 'input is-success' : 'input is-danger']"
+              :class="[nameIsValid ? 'input is-success' : 'input']"
               type="text"
               placeholder="Text input"
             />
+            <span class="icon is-small is-left">
+              <i class="fas fa-user"></i>
+            </span>
+            <span v-if="nameIsValid" class="icon is-small is-right">
+              <i class="fas fa-check"></i>
+            </span>
+            <p v-if="!nameIsValid" class="help is-danger">This field is required</p>
           </div>
         </div>
         <div class="field">
@@ -20,7 +27,7 @@
           <div class="control has-icons-left has-icons-right">
             <input
               v-model="form.email"
-              :class="[emailIsValid ? 'input is-success' : 'input is-danger']"
+              :class="[emailIsValid ? 'input is-success' : 'input']"
               type="email"
               placeholder="Email input"
               value="hello@"
@@ -28,9 +35,10 @@
             <span class="icon is-small is-left">
               <i class="fas fa-envelope"></i>
             </span>
-            <span class="icon is-small is-right">
-              <i class="fas fa-exclamation-triangle"></i>
+            <span v-if="emailIsValid" class="icon is-small is-right">
+              <i class="fas fa-check"></i>
             </span>
+            <p v-if="!emailIsValid" class="help is-danger">This field is required</p>
           </div>
         </div>
 
@@ -43,12 +51,18 @@
 
         <div class="field is-grouped">
           <div class="control">
-            <button @click="submitForm" class="button is-link">Submit</button>
+            <button :disabled="!formIsValid" @click="submitForm" class="button is-link">Send</button>
           </div>
           <div class="control">
             <button class="button is-text">Cancel</button>
           </div>
         </div>
+      </div>
+      <div v-if="form.submitted" class="column is-two-thirds">
+        <h3 class="title is-3">
+          Thank you very much {{form.name}} :)
+          <br />We will contact you soon!
+        </h3>
       </div>
     </div>
   </div>
@@ -65,7 +79,8 @@ export default {
       form: {
         name: null,
         email: null,
-        message: null
+        message: null,
+        submitted: false
       }
     };
   },
@@ -83,7 +98,7 @@ export default {
   methods: {
     submitForm() {
       if (this.formIsValid) {
-        console.log("Form Submitted", this.form);
+        this.$set(this.form, "submitted", true);
       } else {
         console.log("X Invalid form");
       }
